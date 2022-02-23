11fce8b9ea84e7d6d290f9465f394ecc87be7700
Victor Razo <79952018+vmrazo@users.noreply.github.com>
2022-02-22 23:49:51 -0600

    tablas relacionadas con filtro

--- models/get.model.php
+++ models/get.model.php
@@ -32,11 +32,41 @@ class GetModel{
         $relArray = explode(",",$rel);
         $typeArray = explode(",",$type);
 
-        $on1 = $relArray[0].".id_".$typeArray[0];
-        $on2 = $relArray[1].".id_".$typeArray[0]."_".$typeArray[1];
+        //Relacionar dos tablas
+        if(count($relArray) == 2 && count($typeArray) == 2){
+            $on1 = $relArray[0].".id_".$typeArray[1]."_".$typeArray[0];
+            $on2 = $relArray[1].".id_".$typeArray[1];
 
-        $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] inner join $relArray[1] ON $on1 = $on2");
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] inner join $relArray[1] ON $on1 = $on2");
+        }
 
+        //relacionar tres tablas
+        if(count($relArray) == 3 && count($typeArray) == 3){
+
+            $on1a =$relArray[0].".id_".$typeArray[1]."_".$typeArray[0];
+            $on1b =$relArray[1].".id_".$typeArray[1];
+
+            $on2a =$relArray[0].".id_".$typeArray[2]."_".$typeArray[0];
+            $on2b =$relArray[2].".id_".$typeArray[2];
+
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] inner join $relArray[1] ON $on1a = $on1b INNER JOIN
+                    $relArray[2] ON $on2a = $on2b");
+        }
+        //relacionar cuatro tablas
+        if(count($relArray) == 4 && count($typeArray) == 4){
+
+            $on1a =$relArray[0].".id_".$typeArray[1]."_".$typeArray[0];
+            $on1b =$relArray[1].".id_".$typeArray[1];
+
+            $on2a =$relArray[0].".id_".$typeArray[2]."_".$typeArray[0];
+            $on2b =$relArray[2].".id_".$typeArray[2];
+
+            $on3a =$relArray[0].".id_".$typeArray[3]."_".$typeArray[0];
+            $on3b =$relArray[3].".id_".$typeArray[3];
+
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] inner join $relArray[1] ON $on1a = $on1b INNER JOIN
+                    $relArray[2] ON $on2a = $on2b INNER JOIN $relArray[3] ON $on3a = $on3b");
+        }
         $stmt -> execute();
 
         return $stmt->fetchAll(PDO::FETCH_CLASS);
